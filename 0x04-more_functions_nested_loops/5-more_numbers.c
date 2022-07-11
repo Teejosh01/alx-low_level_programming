74c92dfdef921323256ba2df3440cec7121b364f
Teejosh01 <daramolajoshua234@gmail.com>
2022-07-11 01:12:36 -0700

    My 5th task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,16 @@
 #include "main.h"
-
-/**
- * more_numbers - nmbr fct
- * Teejosh01
- * Return: no return
+                                                  /**
+ * more_numbers - nmbr fct                         * Teejosh01                                       * Return: no return
  */
 
-void more_numbers(void)
-{
-
-int i, j;
+void more_numbers(void)                           {
+                                                  int i, j;
 for (i = 1; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j >= 10)
-_putchar('1');
+if (j >= 10)                                      _putchar('1');
 _putchar(j % 10 + '0');
+}                                                 _putchar('\n');
 }
-_putchar('\n');
 }
